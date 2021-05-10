7cb3516762767af00f33686a09c1c2a0caa3e3d1
MfeMeaf <80445981+MfeMeaf@users.noreply.github.com>
2021-05-10 10:24:04 +0200

    v1

---
+++ finland/JS/javascript.js
@@ -0,0 +1,5 @@
+bildArray = [
+"https://www.smartcitiesworld.net/AcuCustom/Sitename/DAM/015/Heksinki_AdobeStock_56312148_rm.jpg",
+"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.bgviaggi.com%2Fimages%2FRovaniemi_2.jpg&f=1&nofb=1",
+"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.lapland.fi%2Fuploads%2F2018%2F03%2Flapland-kemi-finland-snow-castle-winter-kemi-tourism-ltd.jpg&f=1&nofb=1",
+]
